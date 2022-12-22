1eb2197ebefe73cb89d6ee90a284aa1d0f9cff2e
SIVA <sivanandhpp@gmail.com>
2022-12-23 03:23:08 +0530

    Added Google site manager tag

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
 $(function () {
- $(".menu-link").click(function () {
-  $(".menu-link").removeClass("is-active");
-  $(this).addClass("is-active");
- });
+  $(".menu-link").click(function () {
+    $(".menu-link").removeClass("is-active");
+    $(this).addClass("is-active");
+  });
 });
 
 $(function () {
- $(".main-header-link").click(function () {
-  $(".main-header-link").removeClass("is-active");
-  $(this).addClass("is-active");
- });
+  $(".main-header-link").click(function () {
+    $(".main-header-link").removeClass("is-active");
+    $(this).addClass("is-active");
+  });
 });
 
 const dropdowns = document.querySelectorAll(".dropdown");
 dropdowns.forEach((dropdown) => {
- dropdown.addEventListener("click", (e) => {
-  e.stopPropagation();
-  dropdowns.forEach((c) => c.classList.remove("is-active"));
-  dropdown.classList.add("is-active");
- });
+  dropdown.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdowns.forEach((c) => c.classList.remove("is-active"));
+    dropdown.classList.add("is-active");
+  });
 });
 
 $(".search-bar input")
- .focus(function () {
-  $(".header").addClass("wide");
- })
- .blur(function () {
-  $(".header").removeClass("wide");
- });
+  .focus(function () {
+    $(".header").addClass("wide");
+  })
+  .blur(function () {
+    $(".header").removeClass("wide");
+  });
 
 $(document).click(function (e) {
- var container = $(".status-button");
- var dd = $(".dropdown");
- if (!container.is(e.target) && container.has(e.target).length === 0) {
-  dd.removeClass("is-active");
- }
-});
-
-$(function () {
- $(".dropdown").on("click", function (e) {
-  $(".content-wrapper").addClass("overlay");
-  e.stopPropagation();
- });
- $(document).on("click", function (e) {
-  if ($(e.target).is(".dropdown") === false) {
-   $(".content-wrapper").removeClass("overlay");
+  var container = $(".status-button");
+  var dd = $(".dropdown");
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    dd.removeClass("is-active");
   }
- });
 });
 
 $(function () {
- $(".status-button:not(.open)").on("click", function (e) {
-  $(".overlay-app").addClass("is-active");
- });
- $(".pop-up .close").click(function () {
-  $(".overlay-app").removeClass("is-active");
- });
+  $(".dropdown").on("click", function (e) {
+    $(".content-wrapper").addClass("overlay");
+    e.stopPropagation();
+  });
+  $(document).on("click", function (e) {
+    if ($(e.target).is(".dropdown") === false) {
+      $(".content-wrapper").removeClass("overlay");
+    }
+  });
+});
+
+$(function () {
+  $(".status-button:not(.open)").on("click", function (e) {
+    $(".overlay-app").addClass("is-active");
+  });
+  $(".pop-up .close").click(function () {
+    $(".overlay-app").removeClass("is-active");
+  });
 });
 
 $(".status-button:not(.open)").click(function () {
- $(".pop-up").addClass("visible");
+  $(".pop-up").addClass("visible");
 });
 
 $(".pop-up .close").click(function () {
- $(".pop-up").removeClass("visible");
+  $(".pop-up").removeClass("visible");
 });
 
 const toggleButton = document.querySelector('.modeToggle');
@@ -107,23 +107,23 @@ function toggleFullscreen() {
   }
 }
 
-document.getElementById("profile-popup").onclick = function (e) { 
+document.getElementById("profile-popup").onclick = function (e) {
   var x = document.getElementById("profile-dropdown")
-  
+
   if (window.getComputedStyle(x).display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
 }
-document.getElementById("profile-dropdown").onmouseleave = function (e) { 
-  
+document.getElementById("profile-dropdown").onmouseleave = function (e) {
+
   document.getElementById("profile-dropdown").style.display = "none";
 }
 
-document.getElementById("navbar").onclick = function (e) { 
+document.getElementById("navbar").onclick = function (e) {
   var x = document.getElementById("navbarele")
-  
+
   if (window.getComputedStyle(x).display === "none") {
     x.style.display = "block";
   } else {
@@ -131,8 +131,8 @@ document.getElementById("navbar").onclick = function (e) {
   }
 }
 
-$(document).ready(function(){
-	$('#navbar').click(function(){
-		$(this).toggleClass('open');
-	});
+$(document).ready(function () {
+  $('#navbar').click(function () {
+    $(this).toggleClass('open');
+  });
 });
