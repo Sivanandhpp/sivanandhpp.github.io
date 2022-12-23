9047a335cbc914919b7243d8f7d14ddd8f77921b
SIVA <sivanandhpp@gmail.com>
2022-12-23 18:10:01 +0530

    js cleanup and minor changes

--- script.js
+++ script.js
@@ -1,71 +1,22 @@
-$(function () {
-  $(".menu-link").click(function () {
-    $(".menu-link").removeClass("is-active");
-    $(this).addClass("is-active");
-  });
-});
 
-$(function () {
-  $(".main-header-link").click(function () {
-    $(".main-header-link").removeClass("is-active");
-    $(this).addClass("is-active");
-  });
-});
+// TOP RIGHT PROFILE POPUP CONTROLLER 
+document.getElementById("profile-popup").onclick = function (e) {
+  var x = document.getElementById("profile-dropdown")
 
-const dropdowns = document.querySelectorAll(".dropdown");
-dropdowns.forEach((dropdown) => {
-  dropdown.addEventListener("click", (e) => {
-    e.stopPropagation();
-    dropdowns.forEach((c) => c.classList.remove("is-active"));
-    dropdown.classList.add("is-active");
-  });
-});
-
-$(".search-bar input")
-  .focus(function () {
-    $(".header").addClass("wide");
-  })
-  .blur(function () {
-    $(".header").removeClass("wide");
-  });
-
-$(document).click(function (e) {
-  var container = $(".status-button");
-  var dd = $(".dropdown");
-  if (!container.is(e.target) && container.has(e.target).length === 0) {
-    dd.removeClass("is-active");
+  if (window.getComputedStyle(x).display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
-});
+}
+document.getElementById("profile-dropdown").onmouseleave = function (e) {
 
-$(function () {
-  $(".dropdown").on("click", function (e) {
-    $(".content-wrapper").addClass("overlay");
-    e.stopPropagation();
-  });
-  $(document).on("click", function (e) {
-    if ($(e.target).is(".dropdown") === false) {
-      $(".content-wrapper").removeClass("overlay");
-    }
-  });
-});
+  document.getElementById("profile-dropdown").style.display = "none";
+}
 
-$(function () {
-  $(".status-button:not(.open)").on("click", function (e) {
-    $(".overlay-app").addClass("is-active");
-  });
-  $(".pop-up .close").click(function () {
-    $(".overlay-app").removeClass("is-active");
-  });
-});
 
-$(".status-button:not(.open)").click(function () {
-  $(".pop-up").addClass("visible");
-});
 
-$(".pop-up .close").click(function () {
-  $(".pop-up").removeClass("visible");
-});
-
+// LIGHT MODE AND DARK MODE TOGGLE 
 const toggleButton = document.querySelector('.modeToggle');
 
 toggleButton.addEventListener('click', () => {
@@ -74,7 +25,6 @@ toggleButton.addEventListener('click', () => {
 
 // SIDE TAB CHANGE FUNCTION
 function changeTab(evt, tabName) {
-  // Declare all variables
   var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
@@ -94,7 +44,7 @@ function changeTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
-
+// TOP LEFT MacOS BUTTONS CONTROLLER 
 function toggleFullscreen() {
   let elem = document.body;
 
@@ -107,20 +57,8 @@ function toggleFullscreen() {
   }
 }
 
-document.getElementById("profile-popup").onclick = function (e) {
-  var x = document.getElementById("profile-dropdown")
 
-  if (window.getComputedStyle(x).display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-document.getElementById("profile-dropdown").onmouseleave = function (e) {
-
-  document.getElementById("profile-dropdown").style.display = "none";
-}
-
+// TOP LEFT HAMBURGER CONTROLLER ON MOBILE DEVICES 
 document.getElementById("navbar").onclick = function (e) {
   var x = document.getElementById("navbarele")
 
@@ -130,7 +68,7 @@ document.getElementById("navbar").onclick = function (e) {
     x.style.display = "none";
   }
 }
-
+// TOP LEFT HAMBURGER AND CROSS CONTROLLER 
 $(document).ready(function () {
   $('#navbar').click(function () {
     $(this).toggleClass('open');
