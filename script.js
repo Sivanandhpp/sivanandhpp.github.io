67e53129f6664f886524019d8e4b59db2bdeffb7
SIVA <sivanandhpp@gmail.com>
2022-12-24 02:39:45 +0530

    Minor updates and Project integration

--- script.js
+++ script.js
@@ -74,3 +74,20 @@ $(document).ready(function () {
     $(this).toggleClass('open');
   });
 });
+
+
+// COLLAPSIBLE CARD PROJECT WINDOW
+
+var coll = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
